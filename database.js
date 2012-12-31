d769319bfb671387096f768329036878b80dc4a4
Kevin Nuut <sayhello@kevinnuut.com>
2012-12-31 11:55:06 -0500

    monster unique fix

--- database.js
+++ database.js
@@ -7,8 +7,8 @@ exports.init = function(config) {
     dbc = mongodb(config.url, config.collections);
 
     dbc.critter.ensureIndex({seed: 1}, {unique: true});
-    dbc.user.ensureIndex({"fsid": 1},  {unique: true});
-    dbc.user.ensureIndex({"token": 1}, {unique: true});
+    dbc.user.ensureIndex({fsid: 1},  {unique: true});
+    dbc.user.ensureIndex({token: 1}, {unique: true});
     
     return dbc;
 }
